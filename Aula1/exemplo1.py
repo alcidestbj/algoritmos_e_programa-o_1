f5f262b00be0ca9c3ff2af94031d70ec06b532e5
AlcidesJr <8090502+alcidestbj@users.noreply.github.com>
2023-01-26 08:04:00 -0300

    Add files via upload

---
+++ Aula1/exemplo1.py
@@ -0,0 +1,7 @@
+nota1 = float(input("Digite a 1ª nota: "))
+nota2 = float(input("Digite a 2ª nota: "))
+media = (nota1 + nota2) / 2
+if media >= 6:
+    print('Aprovado')
+else:
+    print('Reprovado')
